c87f96b5cb1f8983c8ca1cbf01176b21c96d3ff5
afif ahmed <afifahmed456123@gmail.com>
2021-05-06 13:49:56 +0530

    did some refactoring and naming

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from './pages/Home';
-import NavBar from './components/navbar/index';
+import NavBar from './components/Navbar/index';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function MainComponent(props) {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import BlogCard from "../components/blog/blogcard";
 
 const HomePage = () => {
   return (
     <div>
       <Hero/>
-      <BlogCard/>
     </div>
   );
 };
