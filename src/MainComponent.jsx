64c1e01c1c40e8f5f95ac94751231dd70326c7d3
afif1400 <afifahmed456123@gmail.com>
2021-05-07 15:11:07 +0530

    removed switch import

--- src/MainComponent.jsx
+++ src/MainComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import HomePage from './pages/Home';
 import NavBar from './components/Navbar/index';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default function MainComponent(props) {
   return (
