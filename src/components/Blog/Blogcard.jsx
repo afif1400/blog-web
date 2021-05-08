3240e34558fa8040c9b0d5f1c1e16b61304f8e73
Shivu D G <dgshivu3232@gmail.com>
2021-05-08 13:24:29 +0530

    Successfully fetching blogs from sanity dataset

--- src/components/Blog/Blogcard.jsx
+++ src/components/Blog/Blogcard.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 import '../../styles/blogcard.css';
 
-const BlogCard = () => {
+const BlogCard = ({mainImage, slug, title, abstract}) => {
 	return (
 		<div className='blogcard'>
 			<Card>
 			       	<Card.Img
 					variant='top'
-					src='https://iso.500px.com/wp-content/uploads/2014/12/landscapebest_1-1500x1000.jpg'
+					src={mainImage.asset.url}
+					alt={mainImage.alt}
 				/>
 				<Card.Body>
 					<Card.Title>
-						Almost before we knew it, we had left the ground.
+						{title}
 					</Card.Title>
 					<Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.Some quick example text to build on the
-						card title and make up the bulk of the card's content.
+						{abstract}
 					</Card.Text>
 				</Card.Body>
 				<hr style={{width:"80%", margin:"0 auto"}}/>
-				<Card.Footer>Continue Reading</Card.Footer>
+				<Card.Footer>
+					<Link>
+					</Link>
+				</Card.Footer>
 			</Card>
 		</div>
 	);
